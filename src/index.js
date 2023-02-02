ff9c648f2ec9c67651b9b5e63bec6b787a61b4e6
onio1100 <nonio1100n@gmail.com>
2023-02-02 22:32:16 +0100

    repo start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class TestBody extends React.Component{
+    constructor(props){
+        super(props);
+        this.state  = {
+            button: 0,
+            buttonText: ["CLICK ME", "YOU CLICKED ME", "YOU CLICKED ME AGAIN", "PLEAS DON'T STOP CLICKING ME"],
+        };
+        this.buttonswitch = this.buttonswitch.bind(this);
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    buttonswitch() {
+         let newButton = this.state.button;
+        if(newButton < 3){
+            newButton += 1;
+        }
+        this.setState({button: newButton});
+        console.log(this.state.button);
+    }
+    render(){
+        return(
+            <div className='button' onClick={this.buttonswitch}>{this.state.buttonText[this.state.button]}</div>
+        )
+    }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<TestBody />);
